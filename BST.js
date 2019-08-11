ed2434f1c4741258b8e858dca4c7fcac6354b526
tiennesdm <shubahm.mehta@gmail.com>
2019-08-11 11:05:41 +0530

    BST

---
+++ BST.js
@@ -0,0 +1,68 @@
+///Binary Search Tree
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root == null) {
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while (true) {
+            if (value == current.value) {
+                return undefined;
+            }
+            if (value < current.value) {
+                if (current.left == null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (current.right == null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+    find(value) {
+        if (this.root == null) {
+            return undefined;
+        }
+        var found = false;
+        var current = this.root;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+                // return false;
+            }
+        }
+        if (!found) return undefined;
+        return current;
+    }
+}
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+//console.log(tree.insert(7));
+console.log(tree.find(5));
