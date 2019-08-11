e27bb4b1cad68f6837071965e56c208360e6b266
tiennesdm <shubahm.mehta@gmail.com>
2019-08-11 17:03:07 +0530

    quickSort

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const index = require('./index');
 //console.log(index.mergeSort([10, 24, 76, 45, 73]));
 
-console.log(index.mergeSort([10, 24, 76, 73]));
+console.log(index.quickSort([10, 24, 76, 73]));
--- index.js
+++ index.js
@@ -427,3 +427,31 @@ function merge(arr1, arr2) {
     }
     return result;
 }
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+    const swap = (arr, idx1, idx2) => {
+        return [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    }
+    let pivot = arr[start];
+    let swapidx = start;
+    for (let i = start; i <= end; i++) {
+        if (pivot > arr[i]) {
+            swapidx++;
+            swap(arr, swapidx, i);
+        }
+    }
+    swap(arr, start, swapidx);
+    return swapidx;
+
+
+}
+exports.quickSort = function(arr, start = 0, end = arr.length - 1) {
+    if (start < end) {
+        let pivotIndex = pivot(arr, start, end) //3
+            //left
+        this.quickSort(arr, start, pivotIndex - 1);
+        //right
+        this.quickSort(arr, pivotIndex + 1, end);
+    }
+    return arr;
+}
